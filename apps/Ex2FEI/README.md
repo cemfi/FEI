34c0db499b1000e2a7011c34640e8274f5e9a95a
Artemisch <brenes@designerei-hameln.de>
2019-01-29 10:50:37 +0100

    Added missing image

---
+++ apps/Ex2FEI/README.md
@@ -0,0 +1,50 @@
+*Converting Excel Files to FEI format / Sequence Protocol*
+
+Instructions:
+
+1\. Download the [Ex2Fei Folder](Ex2FEI.zip)
+
+The Folder contains two files:
+
+ SP\_Vorlage (Excel template for the Sequence Protocol) and a Excel\_to\_py\_SP.py (Python App)
+
+![Folder content](../../docs/Docs_FEI.png)
+
+2\. SP\_vorlage (Excel workbook)
+
+Fill the excel template using the data from your sequence protocol (See Protokolle Erklärung.pdf)
+
+Example:
+
+![Excel table](../../docs/Excel_example.png)
+
+
+Notes: Make a new Sheet for every sequence
+
+Important: Be sure that every column containing “Timecode” is formated as text (Menu – Format – Cell format – Text)
+
+3\. Save (save as) the document with the name of your choice.
+
+![image rename](../../docs/rename_template.png )
+
+4\. Open your Terminal window
+
+You find your Terminal window here:
+
+MacOS: applications/utilities/terminal
+
+Using cd command type the path to your downloaded folder to change the directory. Press enter.
+
+![terminal cd](../../docs/cd_directory.png)
+
+Type the following command line:
+Python excel_to_py_SP.py your_excel_file_name.xlsx
+Press enter.
+
+![teminal command line](../../docs/command_line_ex2fei.png)
+
+Open the ex_2_fei Folder.
+A file with your protocols name.json is now there. This is your FEI document.
+
+
+![Image json data](../../docs/json_data.png )
---
+++ apps/Ex2FEI/excel_to_py_SP.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import xlrd
+
+
+
+cwd = os.getcwd()
+cwd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file", help="An Excel file that contains a sequence protocol.")
+args = parser.parse_args()
+#print(args.input_file)
+
+fei = xlrd.open_workbook(args.input_file)
+fp = fei.sheet_by_index(0)
+
+
+def get_sequence_info(sheet):
+    # navigates navigates through sheet, gets values from record's sequences.
+    result = {
+        "summary": str(sheet.cell_value(rowx=1, colx=1)),
+        "timecodestart":str(sheet.cell(rowx=1, colx=2)),
+        "sceneStmt": {
+            "scenes": get_scenes_info(sheet),
+        }
+    }
+    return result
+
+
+def get_scenes_info(sheet):
+    # navigates navigates through sheet, gets values from the scenes sequences.
+    result = []
+    for row_idx in range(1, sheet.nrows):
+        d = {
+            "summary": str(sheet.cell_value(row_idx, colx=4)),
+            "timecodestart":str(sheet.cell_value(row_idx, colx=9)),
+            "reference_frame": str(sheet.cell_value(row_idx, colx=5)),
+            "annotations":[
+                    {
+                        "type": "text",
+                        "about": "content",
+                        "text": str(sheet.cell_value(row_idx, colx=6)),
+                    },
+                    {
+                        "type": "contains_shot",
+                        "number": str(sheet.cell_value(row_idx, colx=8)),
+                    },
+                    {
+                        "type": "text",
+                        "about": "objects",
+                        "text": str(sheet.cell_value(row_idx, colx=7)),
+                    },
+                ],
+
+            }
+        result.append(d)
+    return result
+
+def get_all_sequences(fei):
+    # navigates the workbooks, gets information from all sheets of the workbook
+    result = []
+    for idx in range(fei.nsheets):
+        fp = fei.sheet_by_index(idx)
+        d = get_sequence_info(fp)
+        result.append(d)
+    return result
+
+
+# print(get_all_sequences(fei))
+
+fei_dic = {
+    "fbody":{
+        "protocolStmt":{
+            "content": get_all_sequences(fei),
+        }
+    }
+
+}
+
+file_name=str(args.input_file[:-4]+"json")
+
+
+#print((fei_data[:-4])+"json")
+#print (args.input_file[:-4]+"json")
+#print (file_name)
+
+
+
+#export python library to fei (json data)
+import json
+with open(str(args.input_file[:-4]+"json"), 'w') as file_out:
+    json.dump(fei_dic, file_out, indent=2)
+
+
+#print (str(args.input_file))
+
+
+#print (fei_export)
+
+
+
